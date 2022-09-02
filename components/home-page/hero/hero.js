8b3ede00a42b2c79d8db07c7dde88df719408fb0
vlada-mozgova <vladyslava.m@gmail.com>
2022-09-02 13:03:14 +0300

    add section about me

---
+++ components/home-page/hero/hero.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import classes from "./hero.module.scss";
+
+const Hero = () => {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/vlada.jpg"
+          alt="An image showing Vlada"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Vlada</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        React.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
